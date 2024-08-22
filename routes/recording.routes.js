2cd73bfe2aaef8c989bf223da1221d552d6ba8ab
Berro-1 <hussienberro1@gmail.com>
2024-08-22 15:08:15 +0300

    Added getRecordingById Route

--- routes/recording.routes.js
+++ routes/recording.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getRecordings} from '../controllers/recording.controller.js';
+import { getRecordings, getRecordingById} from '../controllers/recording.controller.js';
 
 const router = express.Router();
 
 // Recording routes
 router.get('/', getRecordings);
+router.get('/:id', getRecordingById);
+
 
 export default router;
