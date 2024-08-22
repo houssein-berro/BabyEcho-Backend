85bac267ebf22fe0ed1e4b53222bd8702bfde441
Berro-1 <hussienberro1@gmail.com>
2024-08-22 15:05:50 +0300

    Added createUser Route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getAllUsers, getUserById } from '../controllers/user.controller.js';
+import { getAllUsers, getUserById, createUser} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
+router.post('/', createUser);
+
 
 // Export the router
 export default router;
