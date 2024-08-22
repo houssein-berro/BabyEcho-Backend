7d221fe200fa1d97d030b240a3b7dc80210ad7f6
Berro-1 <hussienberro1@gmail.com>
2024-08-22 15:11:44 +0300

    Added createDevice Route

--- routes/device.routes.js
+++ routes/device.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getDevices, getDeviceById } from '../controllers/device.controller.js';
+import { getDevices, getDeviceById, createDevice} from '../controllers/device.controller.js';
 
 const router = express.Router();
 
 // Device routes
 router.get('/', getDevices);
 router.get('/:id', getDeviceById);
+router.post('/', createDevice);
 
 
 export default router;
