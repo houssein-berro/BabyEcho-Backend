9cfcf3d0fb4ab513d5d4bda2d904cd530f2d752a
Berro-1 <hussienberro1@gmail.com>
2024-08-22 14:56:25 +0300

    Added connection to mongodb

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import userRoutes from './routes/user.routes.js';
-import recordingRoutes from './routes/recording.routes.js';
-import deviceRoutes from './routes/device.routes.js';
+// import userRoutes from './routes/user.routes.js';
+// import recordingRoutes from './routes/recording.routes.js';
+// import deviceRoutes from './routes/device.routes.js';
 
 dotenv.config();
 
@@ -29,9 +29,9 @@ mongoose
 
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-    app.use('/api/users', userRoutes);
-    app.use('/api/recordings', recordingRoutes);
-    app.use('/api/devices', deviceRoutes);
+    // app.use('/api/users', userRoutes);
+    // app.use('/api/recordings', recordingRoutes);
+    // app.use('/api/devices', deviceRoutes);
  
   })
   .catch(err => console.error('Failed to connect to MongoDB:', err));
