34139f0eacbe0f5787a138bffe01d60fe7aac52c
Berro-1 <hussienberro1@gmail.com>
2024-08-22 15:10:08 +0300

    Added deleteRecording Route

--- routes/recording.routes.js
+++ routes/recording.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getRecordings, getRecordingById, createRecording, updateRecording } from '../controllers/recording.controller.js';
+import { getRecordings, getRecordingById, createRecording, updateRecording, deleteRecording } from '../controllers/recording.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/', getRecordings);
 router.get('/:id', getRecordingById);
 router.post('/', createRecording);
 router.put('/:id', updateRecording);
+router.delete('/:id', deleteRecording);
 
 export default router;
