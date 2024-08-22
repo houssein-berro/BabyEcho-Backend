3e6c184ac389155e0536a3e0f90ff811a346f0a2
Berro-1 <hussienberro1@gmail.com>
2024-08-22 15:09:50 +0300

    Added updateRecording Route

--- routes/recording.routes.js
+++ routes/recording.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getRecordings, getRecordingById, createRecording} from '../controllers/recording.controller.js';
+import { getRecordings, getRecordingById, createRecording, updateRecording } from '../controllers/recording.controller.js';
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', getRecordings);
 router.get('/:id', getRecordingById);
 router.post('/', createRecording);
+router.put('/:id', updateRecording);
 
 export default router;
