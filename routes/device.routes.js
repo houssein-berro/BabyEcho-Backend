245269bc1118e112942fc6f03ed2964430f47385
Berro-1 <hussienberro1@gmail.com>
2024-08-22 15:12:14 +0300

    Added deleteDevice Route

--- routes/device.routes.js
+++ routes/device.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getDevices, getDeviceById, createDevice, updateDevice } from '../controllers/device.controller.js';
+import { getDevices, getDeviceById, createDevice, updateDevice, deleteDevice } from '../controllers/device.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/', getDevices);
 router.get('/:id', getDeviceById);
 router.post('/', createDevice);
 router.put('/:id', updateDevice);
+router.delete('/:id', deleteDevice);
 
 export default router;
