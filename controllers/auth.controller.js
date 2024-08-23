c33f2d8d464002973728ef77b9e8a3a3d56758ee
Berro-1 <hussienberro1@gmail.com>
2024-08-23 10:00:29 +0300

    Updated registeration logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,37 +4,38 @@ import { generateToken } from '../utils/generateToken.js';
 
 // Register a new user
 export const registerUser = async (req, res) => {
-  const { email, password, UserType } = req.body;
-
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+    const { username, email, password, type } = req.body;
+    
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+      
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const newUser = new User({
+        username,
+        email,
+        passwordHash: hashedPassword,
+        type
+      });
+  
+      await newUser.save();
+      res.status(201).json({
+        newUser,
+        token: generateToken(newUser)
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({
-      email,
-      password: hashedPassword,
-      UserType
-    });
-
-    await newUser.save();
-    res.status(201).json({
-      _id: newUser._id,
-      email: newUser.email,
-      UserType: newUser.UserType,
-      token: generateToken(newUser)
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+  };
 
 // User login
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
-
+    console.log(req.body);
+    
   try {
     const user = await User.findOne({ email });
     if (!user) {
@@ -49,7 +50,7 @@ export const loginUser = async (req, res) => {
     res.json({
       _id: user._id,
       email: user.email,
-      UserType: user.UserType,
+      type: user.type,
       token: generateToken(user)
     });
   } catch (error) {
