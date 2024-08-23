dcd4f01c6fbc9639fb314413d25dcbef47ee8a70
Berro-1 <hussienberro1@gmail.com>
2024-08-23 10:03:42 +0300

    Added route to add a baby

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,8 @@ import {
   getAllUsers,
   getUserById,
   updateUser,
-  deleteUser
+  deleteUser,
+  addBabyToUser
 } from '../controllers/user.controller.js';
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get('/', getAllUsers);
 router.get('/:id', getUserById);             
 router.put('/:id', updateUser);              
 router.delete('/:id', deleteUser);                
+router.post('/add-baby/:userId', addBabyToUser);
 
 export default router;
