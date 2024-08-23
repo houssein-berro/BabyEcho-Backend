9d92dcfe8a0b12d0696698025d938e6efe1c991f
Berro-1 <hussienberro1@gmail.com>
2024-08-23 10:29:04 +0300

    Added function to get a recording

--- controllers/recording.controller.js
+++ controllers/recording.controller.js
@@ -1,5 +1,5 @@
-import Recording from '../models/recording.model.js';
-import User from '../models/user.model.js';
+import Recording from "../models/recording.model.js";
+import User from "../models/user.model.js";
 
 // Create recording
 export const createRecording = async (req, res) => {
@@ -13,14 +13,16 @@ export const createRecording = async (req, res) => {
 
     const babyExists = user.babies._id(babyId);
     if (!babyExists) {
-      return res.status(404).json({ message: "Baby not found in user's profile" });
+      return res
+        .status(404)
+        .json({ message: "Baby not found in user's profile" });
     }
 
     const newRecording = new Recording({
       userId,
       babyId,
       duration,
-      recordingURL
+      recordingURL,
     });
 
     await newRecording.save();
@@ -32,10 +34,25 @@ export const createRecording = async (req, res) => {
 
 // Get all recordings
 export const getRecordings = async (req, res) => {
-    try {
-      const recordings = await Recording.find();
-      res.json(recordings);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  try {
+    const recordings = await Recording.find();
+    res.json(recordings);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a single recording by ID
+export const getRecordingById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const recording = await Recording.findById(id);
+    if (!recording) {
+      return res.status(404).json({ message: "Recording not found" });
     }
-  };
+    res.json(recording);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/device.routes.js
+++ routes/device.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-// import { getDevices, getDeviceById, createDevice, updateDevice, deleteDevice } from '../controllers/device.controller.js';
+import { getDevices, getDeviceById, createDevice, updateDevice, deleteDevice } from '../controllers/device.controller.js';
 
 const router = express.Router();
 
-// router.get('/', getDevices);
-// router.get('/:id', getDeviceById);
-// router.post('/', createDevice);
-// router.put('/:id', updateDevice);
-// router.delete('/:id', deleteDevice);
+router.get('/', getDevices);
+router.get('/:id', getDeviceById);
+router.post('/', createDevice);
+router.put('/:id', updateDevice);
+router.delete('/:id', deleteDevice);
 
 export default router;
