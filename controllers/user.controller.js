e2a2a4b0bd1b054fe5378f08bc7f28896ef5b843
Berro-1 <hussienberro1@gmail.com>
2024-08-23 09:37:52 +0300

    Added function to get User by id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,3 +12,17 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+// Get a single user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
