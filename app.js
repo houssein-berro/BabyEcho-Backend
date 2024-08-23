516569df73d8e3d949644ce57188eeab51db9baa
Berro-1 <hussienberro1@gmail.com>
2024-08-23 09:44:44 +0300

    Added base route in app for auth

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
 import recordingRoutes from './routes/recording.routes.js';
 import deviceRoutes from './routes/device.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ mongoose
     app.use('/api/users', userRoutes);
     app.use('/api/recordings', recordingRoutes);
     app.use('/api/devices', deviceRoutes);
- 
+    app.use('/api/auth', authRoutes);
+
   })
   .catch(err => console.error('Failed to connect to MongoDB:', err));
