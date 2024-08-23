c4937a899ea24d6ff4999eb6698dfaade129594c
Berro-1 <hussienberro1@gmail.com>
2024-08-23 10:24:00 +0300

    Adjusted recording model

--- models/recording.model.js
+++ models/recording.model.js
@@ -1,13 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import analysisResultSchema from "./schemas/analysisResult.schema";
 const { Schema } = mongoose;
 
+
 const recordingSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   babyId: { type: Schema.Types.ObjectId, required: true },
   timestamp: { type: Date, default: Date.now },
   duration: Number,
   recordingURL: { type: String, required: true },
-  analysisResults: analysisResultSchema
+  analysisResults: analysisResultSchema,
 });
 
-const Recording = mongoose.model('Recording', recordingSchema);
+const Recording = mongoose.model("Recording", recordingSchema);
 export default Recording;
