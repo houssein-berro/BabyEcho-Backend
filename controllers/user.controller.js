d47e2ea7b8a45274f7ff9f79748a452ac61bcf47
Berro-1 <hussienberro1@gmail.com>
2024-08-23 10:04:39 +0300

    Added function to add a baby

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -49,3 +49,24 @@ export const deleteUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Function to add a baby profile to a user
+export const addBabyToUser = async (req, res) => {
+    const { userId } = req.params;
+    const { name, birthdate, gender, otherDetails } = req.body;
+  
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const newBaby = { name, birthdate, gender, otherDetails };
+      user.babies.push(newBaby);
+      await user.save();
+  
+      res.status(201).json({ message: 'Baby added successfully', babies: user.babies });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
