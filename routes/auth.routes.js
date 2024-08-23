7a08352caff6710e94b0185cb7c604d29c338b53
Berro-1 <hussienberro1@gmail.com>
2024-08-23 09:40:30 +0300

    Added route for logging in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,6 @@ import { registerUser, loginUser } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 export default router;
