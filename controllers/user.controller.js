6bb62e07d502bd3589522d0ffb436e1931c2154a
Berro-1 <hussienberro1@gmail.com>
2024-08-23 09:38:20 +0300

    Added function to update user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -26,3 +26,15 @@ export const getUserById = async (req, res) => {
 };
 
 
+// Update an existing user
+export const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
