ebbdf1cbf327c584c782c160d4aff2f476dc0198
Berro-1 <hussienberro1@gmail.com>
2024-08-23 09:41:59 +0300

    Added function to login user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -31,3 +31,28 @@ export const registerUser = async (req, res) => {
   }
 };
 
+// User login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    res.json({
+      _id: user._id,
+      email: user.email,
+      UserType: user.UserType,
+      token: generateToken(user)
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
