8a7aaa895ac3fab75193f2a136163d6aa13ba7f7
Berro-1 <hussienberro1@gmail.com>
2024-08-23 10:33:27 +0300

    Fixed naming mismatch

--- routes/device.routes.js
+++ routes/device.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getDevices, getDeviceById, createDevice, updateDevice, deleteDevice } from '../controllers/device.controller.js';
+import { getDevicesByUser, getDeviceById, addDevice, updateDevice, deleteDevice } from '../controllers/device.controller.js';
 
 const router = express.Router();
 
-router.get('/', getDevices);
+router.get('/', getDevicesByUser);
 router.get('/:id', getDeviceById);
-router.post('/', createDevice);
+router.post('/', addDevice);
 router.put('/:id', updateDevice);
 router.delete('/:id', deleteDevice);
 
