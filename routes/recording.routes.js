508194c1f9f76eb04368393275a50e72a96bec3d
Berro-1 <hussienberro1@gmail.com>
2024-08-28 02:34:42 +0300

    Adjusted recording routes

--- routes/recording.routes.js
+++ routes/recording.routes.js
@@ -1,12 +1,18 @@
 import express from 'express';
-import { getRecordings, getRecordingById, createRecording, updateRecording, deleteRecording } from '../controllers/recording.controller.js';
+import {
+  getRecordings,
+  getRecordingById,
+  saveRecording,
+  updateRecording,
+  deleteRecording,
+} from '../controllers/recording.controller.js';
 
 const router = express.Router();
 
 router.get('/', getRecordings);
 router.get('/:id', getRecordingById);
-router.post('/', createRecording);
 router.put('/:id', updateRecording);
 router.delete('/:id', deleteRecording);
+router.post('/', saveRecording);
 
 export default router;
