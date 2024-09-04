4b979458542d91408eb1c92f2bf87aa5f88c3b53
Berro-1 <hussienberro1@gmail.com>
2024-09-05 00:04:54 +0300

    Added updateRecordingAnalysis api

--- controllers/recording.controller.js
+++ controllers/recording.controller.js
@@ -114,3 +114,27 @@ export const deleteRecording = async (req, res) => {
   }
 };
 
+export const updateRecordingAnalysis = async (req, res) => {
+  const { id } = req.params;
+  const { resultDetails } = req.body;
+
+  try {
+    // Find the recording by ID and update the analysisResults.resultDetails field
+    const updatedRecording = await Recording.findByIdAndUpdate(
+      id,
+      {
+        $set: { 'analysisResults.resultDetails': resultDetails },
+      },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedRecording) {
+      return res.status(404).json({ message: 'Recording not found' });
+    }
+
+    res.json(updatedRecording);
+  } catch (error) {
+    console.error('Error updating analysis result:', error);
+    res.status(500).json({ message: 'Error updating analysis result' });
+  }
+};
