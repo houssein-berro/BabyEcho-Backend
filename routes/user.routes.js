9c9edc0f35ac6dba0bb5cd68bfc395d87c58081f
Berro-1 <hussienberro1@gmail.com>
2024-09-15 01:57:59 +0300

    Added auth to routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,13 +6,14 @@ import {
   deleteUser,
   addBabyToUser
 } from '../controllers/user.controller.js';
-
+import  authMiddleware  from '../middleware/authMiddleware.js';
+import doctorAuth from '../middleware/doctorAuth.js'
 const router = express.Router();
 
-router.get('/', getAllUsers);                
+router.get('/', doctorAuth ,getAllUsers);                
 router.get('/:id', getUserById);             
 router.put('/:id', updateUser);              
 router.delete('/:id', deleteUser);                
-router.post('/add-baby/:userId', addBabyToUser);
+router.post('/add-baby/:userId', authMiddleware,addBabyToUser);
 
 export default router;
