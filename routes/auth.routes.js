d1bbf9d4eadaf860a6e98a2fea9c33349f6aad24
Berro-1 <hussienberro1@gmail.com>
2024-09-15 01:57:33 +0300

    Added route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { registerUser, loginUser, validateToken } from '../controllers/auth.controller.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
+import { registerUser, loginUser, validateToken, loginDoctor } from '../controllers/auth.controller.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
+router.post('/doctor/login', loginDoctor);
 router.get('/validate', authMiddleware, validateToken);
 
 export default router;
